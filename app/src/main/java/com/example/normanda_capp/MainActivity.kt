676c50e405a79ddd56ef099e3d8742593207349c
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2020-12-10 16:08:45 +0000

    3

--- app/src/main/java/com/example/normanda_capp/MainActivity.kt
+++ app/src/main/java/com/example/normanda_capp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.normanda_capp
 
-import android.os.Bundle
-import android.widget.ImageButton
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.example.normanda_capp.Adaptor.PagerAdaptor
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var  stats:ImageView
     private lateinit var map:ImageView
     private lateinit var wiki:ImageView
-    private lateinit var exercise:ImageView
-    private lateinit var  mPagerAdapter: PagerAdapter
-
+    private lateinit var exercise: ImageView
+    private lateinit var mPagerAdapter: PagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         wiki = findViewById(R.id.wiki)
         exercise = findViewById(R.id.exercise)
 
-        mPagerAdapter = PagerViewAdaptor(supportFragmentManager)
+        mPagerAdapter = PagerAdaptor(supportFragmentManager)
         mViewPager.adapter = mPagerAdapter
         mViewPager.offscreenPageLimit = 5
 
@@ -56,6 +59,22 @@ class MainActivity : AppCompatActivity() {
         mViewPager.currentItem = 0
         stats.setImageResource(R.drawable.ic_baseline_sports_white)
 
+        // CLICK
+        stats.setOnClickListener {
+            mViewPager.currentItem=0
+        }
+        map.setOnClickListener {
+            mViewPager.currentItem=1
+        }
+        wiki.setOnClickListener {
+            mViewPager.currentItem=2
+        }
+        exercise.setOnClickListener {
+            mViewPager.currentItem=3
+        }
+
+
+
     }
 
     private fun changingTabs(position: Int) {
@@ -81,7 +100,7 @@ class MainActivity : AppCompatActivity() {
             wiki.setImageResource(R.drawable.ic_baseline_library_books_white)
             exercise.setImageResource(R.drawable.ic_baseline_fitness)
         }
-        if(position == 1){
+        if(position == 3){
             stats.setImageResource(R.drawable.ic_baseline_sports_kabaddi_24)
             map.setImageResource(R.drawable.ic_baseline_map)
             wiki.setImageResource(R.drawable.ic_baseline_library_books)
